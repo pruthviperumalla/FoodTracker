93ab218adb8773d46260dd94c626a8d5e8a261d0
pruthviperumalla2018 <pruthviperumalla2018@outlook.com>
2018-07-12 21:12:47 +0530

    donor page IDs updated

--- app/src/main/java/com/hackathon/trackers/foodtracker/donor_page.java
+++ app/src/main/java/com/hackathon/trackers/foodtracker/donor_page.java
@@ -9,5 +9,7 @@ public class donor_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donor_page);
+
+
     }
 }
